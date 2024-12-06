6f1ace23cc45cb702926afaf88940be640d2b78d
Boyinbode Ebenezer Ayomide <78053209+Drbenzene@users.noreply.github.com>
2024-12-06 11:35:07 -0500

    updates

--- hooks/useSignIn.js
+++ hooks/useSignIn.js
@@ -3,7 +3,7 @@
 import APICall from "@/services/apiCall";
 
 export async function signIn(values) {
-  const res = await APICall("/auth/login", "POST", values);
+  const res = await APICall("/auth/admin/login", "POST", values);
   console.log(res, "THE RESPONSE");
   localStorage.setItem("accessToken", res?.data.accessToken);
   return res?.data;
